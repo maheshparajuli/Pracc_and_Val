2b2c23443fa3893b9add03c26a44ee7838c10c9f
maheshparajuli <maheshparajuli717@gmail.com>
2025-09-12 23:30:09 +0545

    changes

--- timer.py
+++ timer.py
@@ -5,10 +5,11 @@ def timer(func):
         t_start=time.time()
         result=func(*args,**kargs)
         t_total=time.time()-t_start
-        print("{} took {}s".format(func.__name__,t_total))
+        print("{} took {} seconds".format(func.__name__,t_total))
         return result
     return wrapper
 
+@timer
 def add(a,b):
     print(a+b)
 add(1,2)
