56f141a30812b0b52808a5554490a9833b9e7b0d
maheshparajuli <maheshparajuli717@gmail.com>
2025-09-13 13:44:56 +0545

    skjhdh

--- memoize.py
+++ memoize.py
@@ -1,15 +1,27 @@
+from functools import wraps
 def memoize(func):
     cache={}
+    @wraps(func)
     def wrapper(*args,**kargs):
         krags_key=tuple(sorted(kargs.items()))
 
         if (args,krags_key) not in cache:
+            print("calculating..")
             cache[(args,krags_key)]=func(*args,**kargs)
         return  cache[(args,krags_key)]
     return wrapper
 
 @memoize
-def add(a,b):
+def add(a:int,b:int)->int:
+    """
+    k xa haalchal
+    """
     print(a+b)
 add(20,43)
+
+add.__wrapped__
+
+# print(add.__name__)
+# print(add.__annotations__)
+print(add.__doc__)
 
