5217bb68121b2e500e661b47481cde4acb087e11
maheshparajuli <maheshparajuli717@gmail.com>
2025-09-14 14:18:23 +0545

    oooo

--- memoize.py
+++ memoize.py
@@ -3,7 +3,7 @@ def memoize(func):
     cache={}
     @wraps(func)
     def wrapper(*args,**kargs):
-        krags_key=tuple(sorted(kargs.items()))
+        krags_key=tuple(sorted(kargs.items())) #need to understand this manually
 
         if (args,krags_key) not in cache:
             print("calculating..")
@@ -24,4 +24,5 @@ add.__wrapped__
 # print(add.__name__)
 # print(add.__annotations__)
 print(add.__doc__)
+#still need some more changes to work with it efficiently
 
