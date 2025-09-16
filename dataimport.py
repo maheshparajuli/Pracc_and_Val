7ab14caec777fa91402fb067919c7e0b13046a0e
maheshparajuli <maheshparajuli717@gmail.com>
2025-09-16 17:25:50 +0545

    hj

---
+++ dataimport.py
@@ -0,0 +1,3 @@
+from urllib.request import urlretrieve
+url='https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv'
+urlretrieve(url,'winequality-red')
--- try.py
+++
@@ -1,6 +0,0 @@
-# 2d numpy arrays
-import numpy as np
-
-new=np.array([[1,2,3,4],[1,4,9,16]])
-print(new)
-print(new.shape)
