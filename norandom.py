a9ef8dd80771af7ff3d8897c366df1d776d02208
maheshparajuli <maheshparajuli717@gmail.com>
2025-09-16 14:53:46 +0545

    done

--- norandom.py
+++ norandom.py
@@ -1,4 +1,8 @@
 import numpy as np
 
-height=np.round(np.random.normal(1.75,0.20,5000))
-print(height)
+height=np.round(np.random.normal(1.75,0.20,5000),2)
+weight=np.round(np.random.normal(60.75,15,5000),2)
+
+whole=np.column_stack((height,weight))
+
+print(whole)
