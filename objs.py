45edc41a5d7808e53284d279b7c466edd5217db8
maheshparajuli <maheshparajuli717@gmail.com>
2025-09-23 21:19:45 +0545

    okkj

---
+++ objs.py
@@ -0,0 +1,18 @@
+# class Student:
+#     clz="bright stars"
+
+#     def details(self,cname,cage):
+#         self.name=cname
+#         self.age=cage
+#     def printall(self):
+#         print(f"{self.name} is {self.age} years old")
+
+# c1=Student()
+# c1.details("mahesh",23)
+# c1.printall()
+
+class Bankac:
+    def __init__(self):
+        print("I was called:,But why?")
+s1=Bankac()
+    
