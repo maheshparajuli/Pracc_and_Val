f9e754738b972c7fae534a895e3cfa926420c113
maheshparajuli <maheshparajuli717@gmail.com>
2025-09-24 20:01:38 +0545

    changed inherit.py

--- inherit.py
+++ inherit.py
@@ -6,11 +6,30 @@ class Bankac:
         self.balance-=amount
 
 class Savingsac(Bankac):
-    pass
+    def __init__(self, balance,interest_rate):
+        Bankac.__init__(self,balance)
+        self.interest_rate=interest_rate
 
-ac=Savingsac(200)
+    def compute_interest(self, n_periods=1):
+        return self.balance * ((1 + self.interest_rate) ** n_periods - 1)
+    
+class Checkingac(Bankac):
+    def __init__(self, balance,limit):
+        super().__init__(balance)
+        self.limit=limit
 
-print(isinstance(ac,Bankac))
+    def deposit(self,amount):
+        self.balance+=amount
+
+    def withdraw(self,amount,fee=0):
+        if amount<=self.limit:
+            Bankac.withdraw(self,amount+fee)
+    
+
+ac=Savingsac(300,0.3)
+print(ac.compute_interest(2))
+# print(ac.balance)
+
 
 
         
