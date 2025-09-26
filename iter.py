3306b153936ca895ff93dbc1814531bc3c5144ed
maheshparajuli <maheshparajuli717@gmail.com>
2025-09-26 23:14:57 +0545

    positives everywhere

--- iter.py
+++ iter.py
@@ -15,7 +15,7 @@ while True:
         print(x)
     except StopIteration:
         break
-"""
+
 
 y="myname"
 n=iter(y)
@@ -26,3 +26,41 @@ while True:
     except StopIteration:
         print("loop ended")
         break
+
+class Sequence:
+    def __iter__(self):
+        self.a=1
+        return self
+    def __next__(self):
+        x=self.a
+        self.a+=1
+        return x
+obj=Sequence()
+obj.__iter__()
+print(obj.__next__())
+print(obj.__next__())
+
+"""
+class Sequence:
+    def __iter__(self):
+        self.a=1
+        return self  # returning self.a would give typeerror because it is of type int which is non iterator
+
+    def __next__(self):
+        if self.a<=20:
+            x=self.a
+            self.a+=x
+            # return x
+            return x
+        else:
+            raise StopIteration
+see=Sequence()
+my=iter(see)
+
+print(next(my))  
+print(next(my))    
+
+    
+
+
+
