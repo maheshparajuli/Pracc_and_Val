b05efb44fe5b0b4b4a3e21c45b43de2dc3737f76
maheshparajuli <maheshparajuli717@gmail.com>
2025-09-27 22:13:37 +0545

    c

---
+++ ploy.py
@@ -0,0 +1,39 @@
+''' The word polymorphism means many forms, and it means methods/functions/operators 
+with the same name that can be executed on many objects or classes.
+
+#function polymorphism:
+len() function can be used in different objects i.e iterable objects
+example:
+lst=list(1,2,4,8)
+print(len(lst))
+
+#class polymorphism:
+Polymorphism is often used in Class methods, where we can have multiple classes with the same method name.
+
+
+#more than simple example
+
+class Teacher():
+    def greet(self):
+        print("aey budo, namaste hai")
+class Student():
+    def greet(self):
+        print("k xa bro?")
+ob1=Teacher()
+ob2=Student()
+
+for i in (ob1,ob2):
+    i.greet()
+
+#Inheritance Class Polymorphism
+nothing but just checking what happens when we define same named methods in parent and child classes, the result is that the child class 
+methods can override parent class methods(same name wala)
+
+lets demonstrate with example:
+
+'''
+class School:
+    def name(self):
+        print("Bright Angels' School, Thankot")
+class Student():
+    
