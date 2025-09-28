745f9cf11b671ac0aeb96c34c58e29ed0c388955
maheshparajuli <maheshparajuli717@gmail.com>
2025-09-28 18:04:43 +0545

    done

--- so.py
+++ so.py
@@ -1,35 +1,37 @@
+
+
 class Book:
-    def __init__(self, title):
-        self.title = title
-        self.is_borrowed = False
+    def __init__(self,title):
+        self.title=title
+        self.is_borrowed=False
 
 class Library:
     def __init__(self):
-        self.books = []
-    
-    def add_books(self, titles):
+        self.books=[]
+
+    def add_books(self,titles):
         for t in titles:
-            self.books.append(Book(t))
-    
-    def borrow_book(self, title):
+            self.books.append(Book(t))  # self.books=Book("title") garya jasto ho yo
+    def borrow_books(self,title):
         for book in self.books:
-            if book.title == title and not book.is_borrowed:
-                book.is_borrowed = True
-                print(f"You borrowed '{title}'")
-                return
-        print(f"'{title}' is not available")
+            if book.title==title and book.is_borrowed==False:
+                self.is_borrowed=True
+                print(f"{book.title} is borrowed")
+            return
+        print(f"{book.title} is not available")
     
+
     def display_books(self):
-        print("\nLibrary Collection:")
+        print("Library collection:")
         for book in self.books:
-            status = "Available" if not book.is_borrowed else "Borrowed"
-            print(f"{book.title} -> {status}")
+            status="Available" if not book.is_borrowed else "Borrowed"
+            print(f"{book.title}->{status}")
+
+o1=Library()
+o1.add_books(["Real Analysis",'matrix theory','modern algebra'])
+# o1.display_books()
+
+o1.borrow_books("Real Analysis")
+o1.display_books()
 
 
-# Example
-lib = Library()
-lib.add_books(["Python Basics", "Data Science", "AI with Python"])
-lib.display_books()
-
-lib.borrow_book("Python Basics")
-lib.display_books()
